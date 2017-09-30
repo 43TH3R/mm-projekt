eacae2833f7c383c450d70ad83bb3493d703b0c4
Michal Fikar <michal.fikar@gmail.com>
2017-09-30 14:25:38 +0200

    add random button onclick

--- script.js
+++ script.js
@@ -12,34 +12,45 @@ canvas.setAttribute("height", canvasH);
 var c = canvas.getContext("2d");
 c.strokeStyle = "#000000";
 
-boxes = [];
-grid = [];
-for (var i=0; i<gridW; i++) {
-	var row = [];
-	for (var j=0; j<gridH; j++) row.push([]); 
-	grid.push(row);
-}
+var boxes = [];
+// var grid = [];
+// for (var i=0; i<gridW; i++) {
+// 	var row = [];
+// 	for (var j=0; j<gridH; j++) row.push([]);
+// 	grid.push(row);
+// }
 
-var addBox = function() {
-	newW = Math.random()*80 + 20;
-	newH = Math.random()*80 + 20;
+function addBox() {
+	newW = Math.random()*70 + 30;
+	newH = Math.random()*70 + 30;
 	newX = Math.random()*(canvasW - newW);
 	newY = Math.random()*(canvasH - newH);
 	newVx = Math.random()*10 - 5;
 	newVy = Math.random()*10 - 5;
 	boxes.push({x: newX, y: newY, w: newW, h: newH, vx: newVx, vy: newVy});
-	gridX1 = Math.floor(newX/gridSize);
-	gridY1 = Math.floor(newY/gridSize);
-	gridX2 = Math.floor((newX + newW)/gridSize);
-	gridY2 = Math.floor((newY + newH)/gridSize);
-	grid[gridX1][gridY1].push(boxes.length - 1);
-	grid[gridX2][gridY2].push(boxes.length - 1);
-	for (var i=gridX1; i<gridX2; i++) {
-		for (var j=gridY1; j<gridY2; i++) grid[i][j] = boxes.length - 1;
-	}
+	// gridX1 = Math.floor(newX/gridSize);
+	// gridY1 = Math.floor(newY/gridSize);
+	// gridX2 = Math.floor((newX + newW)/gridSize);
+	// gridY2 = Math.floor((newY + newH)/gridSize);
+	// for (var i=gridX1; i<=gridX2; i++) {
+	// 	for (var j=gridY1; j<=gridY2; i++) grid[i][j] = boxes.length - 1;
+	// }
 }
 
-var drawGrid = function() {
+// function printGrid() {
+//     for (var i = 0; i < gridW; i++) {
+//         var riadok = "";
+//         for (var j = 0; j < gridH; j++) {
+//             for (var k = 0; k < grid[i][j].length; k++) {
+//                 riadok += grid[i][j][k] + " ";
+//             }
+//             riadok += "|";
+//         }
+//         console.log(riadok);
+//     }
+// }
+
+function drawGrid() {
 	for (var i=1; i<gridH; i++) {
 		c.moveTo(0, i*gridSize);
 		c.lineTo(canvasW, i*gridSize);
@@ -52,19 +63,24 @@ var drawGrid = function() {
 	}
 }
 
-var draw = function() {
+function draw() {
     c.clearRect(0, 0, canvasW, canvasH, 0.5);
 	// drawGrid();
-	boxes.forEach(function(box, i) {
-		c.fillStyle = "#000000";
-		c.fillRect(box.x, box.y, box.w, box.h);
-		c.fillStyle = "#ff0000";
-		c.font="20px Arial";
-		c.fillText(i, box.x + box.w/2 - 5, box.y + box.h/2 + 5);
-	});
+    drawBoxes();
 }
 
-var move = function() {
+function drawBoxes() {
+    boxes.forEach(function(box, i) {
+        c.fillStyle = "#000000";
+        c.fillRect(box.x, box.y, box.w, box.h);
+        c.fillStyle = "#ff0000";
+        c.font="20px Arial";
+        var label = "" + (i+1);
+        c.fillText(label, box.x + box.w/2 - c.measureText(label).width/2, box.y + box.h/2 + 7);
+    });
+}
+
+function move() {
 	boxes.forEach(function(box) {
 		if (box.x + box.vx < 0 || box.x + box.w + box.vx > canvasW) box.vx = -box.vx;
 		if (box.y + box.vy < 0 || box.y + box.h + box.vy > canvasH) box.vy = -box.vy;
@@ -73,13 +89,33 @@ var move = function() {
 	});
 }
 
-var mainLoop = function() {
+function mainLoop() {
+    move();
 	draw();
-	move();
 }
 
 addBox();
 addBox();
-console.log(grid);
-	
-setInterval(mainLoop, 16);
+addBox();
+addBox();
+
+var interval;
+var playing = false;
+
+function start() {
+    if (!playing) {
+        interval = setInterval(mainLoop, 16);
+        playing = true;
+    }
+}
+
+function stop() {
+    if (playing) {
+        clearInterval(interval);
+        drawGrid();
+        drawBoxes();
+        playing = false;
+    }
+}
+
+start();
