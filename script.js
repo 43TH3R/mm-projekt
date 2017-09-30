6a22b704c94840cf996660b4dea24fbf70b6f477
Michal Fikar <michal.fikar@gmail.com>
2017-09-30 11:10:33 +0200

    add constants

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-console.log("script");
+// constants
+const gridSize = 100;
+const gridW = 8;
+const gridH = 6;
+const canvasW = gridW * gridSize;
+const canvasH = gridH * gridSize;
+
+// initialization
 var canvas = document.getElementById("canvas");
+canvas.setAttribute("width", canvasW);
+canvas.setAttribute("height", canvasH);
+
 var c = canvas.getContext("2d");
 c.moveTo(0,0);
 c.lineTo(1000,1000);
 c.stroke();
+
+setInterval()
