f67fa6e7a743eb4399bf3cfae2500c940857e700
Michal Fikar <michal.fikar@gmail.com>
2017-12-18 15:17:53 +0100

    Improve box update, list collisions

--- script.js
+++ script.js
@@ -62,8 +62,36 @@ function removeBoxById(i) {
     boxes = boxes.filter(box => box.i !== i);
     for (let x = 0; x < gridW; x++) {
         for (let y = 0; y < gridH; y++) {
-            if (grid[x][y].hasOwnProperty(i)) delete grid[x][y][i];
+            removeBoxFromCell({i: i}, grid, x, y);
         }
+    }
+}
+
+let collisions = {};
+
+function addCollision(b1, b2) {
+    if (b1 == b2) return;
+    b1 += ''; b2 += '';
+    if (!collisions.hasOwnProperty(b1)) {
+        collisions[b1] = new Set([b2]);
+    } else {
+        collisions[b1].add(b2);
+    }
+    if (!collisions.hasOwnProperty(b2)) {
+        collisions[b2] = new Set([b1]);
+    } else {
+        collisions[b2].add(b1);
+    }
+}
+
+function removeColision(b1, b2) {
+    if (b1 == b2) return;
+    b1 += ''; b2 += '';
+    if (collisions.hasOwnProperty(b1)) {
+        collisions[b1].delete(b2);
+    }
+    if (collisions.hasOwnProperty(b2)) {
+        collisions[b2].delete(b1);
     }
 }
 
@@ -71,8 +99,67 @@ function addBoxToGrid(box, grid) {
     const cr = getBoxCellRange(box);
     for (let x = cr.a.x; x <= cr.b.x; x++) {
         for (let y = cr.a.y; y <= cr.b.y; y++) {
-            grid[x][y][box.i] = true;
+            addBoxToCell(box, grid, x, y);
         }
+    }
+}
+
+function addBoxToCell(box, grid, x, y) {
+    grid[x][y][box.i] = true;
+}
+
+function removeBoxFromCell(box, grid, x, y) {
+    if (grid[x][y].hasOwnProperty(box.i)) delete grid[x][y][box.i];
+}
+
+function updateBoxOnGrid(box, grid) {
+    const prev = getBoxCellRange(box)
+    box.x = box.x + box.vx;
+    box.y = box.y + box.vy;
+    const next = getBoxCellRange(box);
+    if (prev.a.x < next.a.x) {
+        // lava hrana sa pohla do prava - odstranit stlpec z lava
+        for (let y = prev.a.y; y <= prev.b.y; y++)
+            for (let x = prev.a.x; x < next.a.x; x++)
+                removeBoxFromCell(box, grid, x, y);
+    } else if (prev.a.x > next.a.x) {
+        // lava hrana sa pohla do lava - pridat stlpec z lava
+        for (let y = next.a.y; y <= next.b.y; y++)
+            for (let x = next.a.x; x < prev.a.x; x++)
+                addBoxToCell(box, grid, x, y);
+    }
+    if (prev.b.x < next.b.x) {
+        // prava hrana sa pohla do prava - pridat stlpec z prava
+        for (let y = next.a.y; y <= next.b.y; y++)
+            for (let x = prev.b.x+1; x <= next.b.x; x++)
+                addBoxToCell(box, grid, x, y);
+    } else if (prev.b.x > next.b.x) {
+        // prava hrana sa pohla do lava - odstranit stlpec z prava
+        for (let y = prev.a.y; y <= prev.b.y; y++)
+            for (let x = next.b.x+1; x <= prev.b.x; x++)
+                removeBoxFromCell(box, grid, x, y);
+    }
+    if (prev.a.y > next.a.y) {
+        // horna hrana sa pohla hore - pridat riadok z hora
+        for (let x = next.a.x; x <= next.b.x; x++)
+            for (let y = next.a.y; y < prev.a.y; y++)
+                addBoxToCell(box, grid, x, y);
+    } else if (prev.a.y < next.a.y) {
+        // horna hrana sa pohla dole - odstranit riadok z hora
+        for (let x = prev.a.x; x <= prev.b.x; x++)
+            for (let y = prev.a.y; y < next.a.y; y++)
+                removeBoxFromCell(box, grid, x, y);
+    }
+    if (prev.b.y > next.b.y) {
+        // dolna hrana sa pohla hore - odstranit riadok z dola
+        for (let x = prev.a.x; x <= prev.b.x; x++)
+            for (let y = next.b.y+1; y <= prev.b.y; y++)
+                removeBoxFromCell(box, grid, x, y);
+    } else if (prev.b.y < next.b.y) {
+        // dolna hrana sa pohla dole - pridat riadok z dola
+        for (let x = next.a.x; x <= next.b.x; x++)
+            for (let y = prev.b.y+1; y <= next.b.y; y++)
+                addBoxToCell(box, grid, x, y);
     }
 }
 
@@ -80,7 +167,7 @@ function removeBoxFromGrid(box, grid) {
     const cr = getBoxCellRange(box);
     for (let x = cr.a.x; x <= cr.b.x; x++) {
         for (let y = cr.a.y; y <= cr.b.y; y++) {
-            grid[x][y][box.i] = false;
+            removeBoxFromCell(box, grid, x, y);
         }
     }
 }
@@ -89,9 +176,21 @@ function printGrid() {
     let s = '';
     for (let y = 0; y < gridH; y++) {
         for (let x = 0; x < gridW; x++) {
+            let b = false;
             for (let i = 0; i < boxes.length; i++) {
-                s += grid[x][y][boxes[i].i] ? i : ' ';
+                // s += grid[x][y][boxes[i].i] ? i : ' ';
+                if (grid[x][y][boxes[i].i]) {
+                    if (!b) {
+                        b = boxes[i].i
+                    } else {
+                        b = "X";
+                        break;
+                    }
+                }
+
             }
+            if (!b) s += ' ';
+            else s += b;
             s += ' ';
         }
         s += '\n';
@@ -162,16 +261,44 @@ function move() {
 		if (box.x + box.vx < 0 || box.x + box.w + box.vx > canvasW) box.vx = -box.vx;
 		if (box.y + box.vy < 0 || box.y + box.h + box.vy > canvasH) box.vy = -box.vy;
 		else box.vy = box.vy + box.w*box.h*density; 
-		removeBoxFromGrid(box, grid);
-		box.x = box.x + box.vx;
-		box.y = box.y + box.vy;
-        addBoxToGrid(box, grid);
+		updateBoxOnGrid(box, grid);
 	});
 }
 
+function checkCollisions() {
+    collisions = {};
+    for (let x = 0; x < gridW; x++) {
+        for (let y = 0; y < gridH; y++) {
+            Object.keys(grid[x][y]).forEach(b1 =>
+                Object.keys(grid[x][y]).forEach(b2 => addCollision(b1, b2)));
+        }
+    }
+}
+
+let times = [];
+
 function mainLoop() {
+    var start = performance.now();
     move();
+    checkCollisions();
 	draw();
+	var end = performance.now();
+
+	// time calc
+	times.push(end-start);
+	if (times.length > 60) times.splice(0,1);
+	document.getElementById('frametime').innerHTML = ("" + (end - start)).substr(0,7);
+    const sum = times.reduce((a,v) => a + v);
+    document.getElementById('frametime-ma').innerHTML = ("" + (sum / times.length)).substr(0,7);
+
+    // print collisions
+    let colText = '';
+    boxes.forEach(function (b1) {
+       colText += b1.i + ':';
+       if (collisions.hasOwnProperty(b1.i)) collisions[b1.i].forEach(b2 => colText += ' '+b2);
+       colText += '\n';
+    });
+    document.getElementById('collisions').innerHTML = colText;
 }
 
 // playback control
@@ -195,7 +322,8 @@ function stop() {
 function step() {
 	if (!playing) {
         mainLoop();
-        printGrid();
+        // printGrid();
+        // console.log(collisions);
     }
 }
 
@@ -214,4 +342,4 @@ addBox();
 addBox();
 addBox();
 
-start();
+// start();
